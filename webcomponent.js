5d91c74e31a7e5ca4e49e4459772249a58506a11
Ashutosh <ashutoshrai181094@gmail.com>
2020-08-06 13:22:15 +0530

    Create webcomponent.js

---
+++ webcomponent.js
@@ -0,0 +1,241 @@
+(function()  {
+    
+      
+    const highChartJs = "https://code.highcharts.com/highcharts.js";
+    const highChart3DJs = "https://code.highcharts.com/highcharts-3d.js";
+    const highChartsCylinderJS = "https://code.highcharts.com/modules/cylinder.js";
+    const highChartsExportingJS="https://code.highcharts.com/modules/exporting.js";
+    const highChartsExportDataJS="https://code.highcharts.com/modules/export-data.js";
+    const highChartsAccessiblityJS="https://code.highcharts.com/modules/accessibility.js";
+    
+    console.log("1-Step");
+    
+    //This function is used to load the library
+    
+    function loadScript(src)
+    {
+    
+    console.log("Step-9");
+    
+	  return new Promise(function(resolve, reject) 
+    {
+		let script = document.createElement('script');
+		script.src = src;
+
+		script.onload = () => {console.log("Load: " + src); resolve(script);}
+		script.onerror = () => reject(new Error(`Script load error for ${src}`));
+
+		document.head.appendChild(script)
+	  });
+    
+	}
+    
+    
+    console.log("Step-2");
+    
+  let template = document.createElement('template');
+  template.innerHTML = `
+  <style>
+#container {
+    height: 400px; 
+}
+
+.highcharts-figure, .highcharts-data-table table {
+    min-width: 310px; 
+    max-width: 800px;
+    margin: 1em auto;
+}
+
+.highcharts-data-table table {
+    font-family: Verdana, sans-serif;
+    border-collapse: collapse;
+    border: 1px solid #EBEBEB;
+    margin: 10px auto;
+    text-align: center;
+    width: 100%;
+    max-width: 500px;
+}
+.highcharts-data-table caption {
+    padding: 1em 0;
+    font-size: 1.2em;
+    color: #555;
+}
+.highcharts-data-table th {
+	font-weight: 600;
+    padding: 0.5em;
+}
+.highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
+    padding: 0.5em;
+}
+.highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
+    background: #f8f8f8;
+}
+.highcharts-data-table tr:hover {
+    background: #f1f7ff;
+}
+</style>
+<figure class="highcharts-figure">
+    <div id="container">
+    </div>
+</figure>
+  `;
+  
+  console.log("Step3");
+
+    
+   
+  
+  customElements.define('com-sap-sample-helloworld5', class HelloWorld extends HTMLElement     {
+   constructor() {
+			super(); 
+      
+      console.log("step-4");
+		   let shadowRoot = this.attachShadow({mode: "open"});
+		   shadowRoot.appendChild(template.content.cloneNode(true));
+       this._firstConnection = false;
+           
+           
+        
+
+      this.addEventListener("click", event => {
+				var event = new Event("onClick");
+				this.dispatchEvent(event);
+            });
+		
+    
+    }
+
+        //Fired when the widget is added to the html DOM of the page
+        connectedCallback(){
+        
+        console.log("Step-5");
+        this._firstConnection = true;
+        
+        async function LoadLibs(callme) {
+        console.log("Step - 7");
+        
+					try
+          {
+						console.log("Step-8");
+						await loadScript(highChartJs);				
+						await loadScript(highChart3DJs);		
+            await loadScript(highChartsCylinderJS);
+						await loadScript(highChartsExportingJS);
+            await loadScript(highChartsExportDataJS);
+            await loadScript(highChartsAccessiblityJS);
+            
+					} 
+          catch (e) 
+          {
+						alert(e);
+					} 
+          finally 
+          {
+          console.log("Step-10");
+          callme.redraw();
+					}
+				}
+        
+        console.log("Step-6");
+        LoadLibs(this);
+        }
+        
+       
+       
+       
+
+
+         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+        disconnectedCallback(){
+        
+        }
+
+         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+		onCustomWidgetBeforeUpdate(oChangedProperties) {
+    
+    
+
+		}
+    
+    
+
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+		onCustomWidgetAfterUpdate(oChangedProperties) {
+            if (this._firstConnection){
+            this.redraw();
+            }
+            
+            
+        }
+        
+        //When the custom widget is removed from the canvas or the analytic application is closed
+        onCustomWidgetDestroy(){
+        
+        }
+
+        
+        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+        // Commented out by default
+        /*
+        onCustomWidgetResize(width, height){
+        
+        }
+        */
+         
+
+         get chartType() {
+            return this.chartTypeValue;
+        }
+
+        set chartType(value) {
+            this.chartTypeValue = value;
+        }
+        
+     
+        
+
+
+      redraw()
+        {
+        let myChart=this.shadowRoot.getElementById('container');
+        console.log("Step-11");
+        
+       
+      Highcharts.chart(myChart, {
+      chart: {
+        type: 'cylinder',
+        options3d: {
+            enabled: true,
+            alpha: 15,
+            beta: 15,
+            depth: 50,
+            viewDistance: 25
+        }
+    },
+    title: {
+        text: 'Highcharts Cylinder Chart'
+    },
+    plotOptions: {
+        series: {
+            depth: 25,
+            colorByPoint: true
+        }
+    },
+    series: [{
+        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+        name: 'Cylinders',
+        showInLegend: false
+    }]
+});
+
+        
+        
+   
+
+        }
+    
+    
+    });
+    
+   
+})();
